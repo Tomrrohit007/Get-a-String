2c655d96510932e23ba5c620a731fd489c43496b
Tomrrohit007 <102895047+Tomrrohit007@users.noreply.github.com>
2022-11-16 12:50:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  response.send("Express Js");
+  response.send("Express JS");
 });
 
 app.listen(3000);
